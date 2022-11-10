6a54ec73beccade3e7f6f47eeb51ac8663730488
dnnijmlinn <den.koski@icloud.com>
2022-11-10 18:27:09 +0200

    WikiSearch

---
+++ wiki/backend/Api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var cors = require("cors");
+const { wikisearch } = require("./wikisearch");
+
+const app = express();
+app.use(cors());
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
+
+app.get("/hello", (req, res, next) => {
+  res.end("Hello world!");
+});
+
+app.get("/wiki", (req, res, next) => {
+  wikisearch(req.query.haku, (error, data) => {
+    if (error) {
+      res.end(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
